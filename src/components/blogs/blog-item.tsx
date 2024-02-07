5aef72693e36a7ad0577650a7de56146d28309e9
usama444 <usama444@hotmail.com>
2024-02-07 10:51:12 +0500

    update theme

--- src/components/blogs/blog-item.tsx
+++ src/components/blogs/blog-item.tsx
@@ -9,10 +9,18 @@ export default function BlogsItem({ blog }: IBlogItemProps) {
   const { id, title, url } = blog;
   const date = getRandomDate().toDateString();
   return (
-    <Card isBlurred>
+    <Card
+      isBlurred
+      className="border-none bg-background/60 dark:bg-default-100/50 "
+      shadow="sm"
+    >
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny  uppercase font-bold">Blog #{id}</p>
-        <h4 className="text-white font-medium text-large">{title}</h4>
+        <p className="text-tiny light:font-bold light:text-black uppercase font-bold">
+          Blog #{id}
+        </p>
+        <h4 className="text-black dark:text-white font-medium text-large">
+          {title}
+        </h4>
       </CardHeader>
       <Image
         removeWrapper
@@ -21,7 +29,7 @@ export default function BlogsItem({ blog }: IBlogItemProps) {
         src={url}
       />
       <CardFooter className="absolute bg-black/40 bottom-0 z-10 ">
-        <p className="text-tiny text-white/80">{date}</p>
+        <p className="text-tiny text-white">{date}</p>
       </CardFooter>
     </Card>
   );
