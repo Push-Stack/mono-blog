f67d7e3ff6ce7e78a6705e2cc03e2896a825b290
usama444 <usama444@hotmail.com>
2024-02-07 10:23:16 +0500

    update api method

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,10 @@ export const getBlogs = async () => {
   try {
     const response = await fetch(`${BASE_URL}/photos`);
     const data = (await response.json()) as Blog[];
-    return data;
+
+    // We're trimming the array because the JSONPlaceholder API lacks pagination support. Showing all 5000 photos simultaneously would strain performance, so we need to use list virtualization. However, implementing this feature isn't within the scope of the current page.
+
+    return data.slice(0, 25);
   } catch (error) {
     return [];
   }
